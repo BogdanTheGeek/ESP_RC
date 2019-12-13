679ba8e522d773f16ebe85aace96d29843b8e0b3
BogdanTheGeek <bobby.b.tiger@gmail.com>
2019-12-13 22:59:38 +0000

    ctrl-s

--- src/Transmitter/Transmitter.ino
+++ src/Transmitter/Transmitter.ino
@@ -1,25 +1,42 @@
 #include "libs.h" 
 NowConnection *connection;
 
+
+typedef struct {
+	uint8_t ch0_pos;
+	uint8_t ch1_pos;
+	uint8_t ch2_pos;
+	uint8_t ch3_pos;
+	uint8_t ch4_pos;
+	uint8_t ch5_pos;
+
+}CONTROLS;
+
+CONTROLS position_data;
+
 void setup(){
 
-	#ifdef(DEBUG_OUT)
+	#if defined(DEBUG_OUT)
 	Serial.begin(9600);
 	#endif
 	connection = new NowConnection();
 	delay(500);
 
-	#ifdef(DEBUG_OUT)
+	#if defined(DEBUG_OUT)
 	String mac = connection->my_mac();
 	Serial.println("My MAC:");
 	Serial.println(mac);
 	#endif
+
+	char* out = connection->get_pkt_out();
+	memcpy(out, &position_data, sizeof(position_data));
+
 }
 
 void loop(){
 
 	connection->send();
-	#ifdef(DEBUG_OUT)
+	#if defined(DEBUG_OUT)
 	Serial.println("Sent");
 	#endif
 	delay(1000);
